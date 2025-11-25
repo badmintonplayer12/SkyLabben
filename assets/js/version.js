8f47acfad31ba3e0ce6fb4a81bb0a69add03c58e
badmintonplayer12 <lea.garpestad@gmail.com>
2025-11-25 07:25:07 +0100

    bad image

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Felles versjonskilde for service worker og data-cache
 // Oppdateres av scripts/update-version.js (kort hash + dato er nok)
-export const APP_VERSION = '2025-11-25+003815-704c349';
+export const APP_VERSION = '2025-11-25+072507-4db97e4';
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CACHE_VERSION oppdateres av scripts/update-version.js
-const CACHE_VERSION = '2025-11-25+003815-704c349';
+const CACHE_VERSION = '2025-11-25+072507-4db97e4';
 
 const PRECACHE_PATHS = [
   './',
