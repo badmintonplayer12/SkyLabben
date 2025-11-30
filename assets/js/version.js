1cf7d3071df567d518de46529987327a65d27ddf
badmintonplayer12 <lea.garpestad@gmail.com>
2025-11-30 21:50:23 +0100

    rename + garasje

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Felles versjonskilde for service worker og data-cache
 // Oppdateres av scripts/update-version.js (kort hash + dato er nok)
-export const APP_VERSION = '2025-11-27+003034-1ead488';
+export const APP_VERSION = '2025-11-30+215023-08c833a';
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CACHE_VERSION oppdateres av scripts/update-version.js
-const CACHE_VERSION = '2025-11-27+003034-1ead488';
+const CACHE_VERSION = '2025-11-30+215023-08c833a';
 
 const PRECACHE_PATHS = [
   './',
