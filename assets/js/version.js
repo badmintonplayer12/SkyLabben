400d5ce8bc156419d07aac9f9f87ea8f5564c622
badmintonplayer12 <lea.garpestad@gmail.com>
2025-11-30 22:00:10 +0100

    fikjs

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Felles versjonskilde for service worker og data-cache
 // Oppdateres av scripts/update-version.js (kort hash + dato er nok)
-export const APP_VERSION = '2025-11-30+215430-46c9ac1';
+export const APP_VERSION = '2025-11-30+220010-b0344a6';
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CACHE_VERSION oppdateres av scripts/update-version.js
-const CACHE_VERSION = '2025-11-30+215430-46c9ac1';
+const CACHE_VERSION = '2025-11-30+220010-b0344a6';
 
 const PRECACHE_PATHS = [
   './',
