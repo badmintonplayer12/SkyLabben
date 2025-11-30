46c9ac1b0e1f857f63d8cbe18d21ca9446d1823c
badmintonplayer12 <lea.garpestad@gmail.com>
2025-11-30 21:53:23 +0100

    remove approvedbydefault all projects

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Felles versjonskilde for service worker og data-cache
 // Oppdateres av scripts/update-version.js (kort hash + dato er nok)
-export const APP_VERSION = '2025-11-30+215023-08c833a';
+export const APP_VERSION = '2025-11-30+215324-1cf7d30';
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CACHE_VERSION oppdateres av scripts/update-version.js
-const CACHE_VERSION = '2025-11-30+215023-08c833a';
+const CACHE_VERSION = '2025-11-30+215324-1cf7d30';
 
 const PRECACHE_PATHS = [
   './',
