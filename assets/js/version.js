8f763c235f674f6cfb27a29d9b0554e5c5fe302a
badmintonplayer12 <lea.garpestad@gmail.com>
2025-12-06 18:30:00 +0100

    new cover

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,3 +1,3 @@
 // Felles versjonskilde for service worker og data-cache
 // Oppdateres av scripts/update-version.js (kort hash + dato er nok)
-export const APP_VERSION = '2025-12-06+182614-53991ec';
+export const APP_VERSION = '2025-12-06+183000-3188b59';
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CACHE_VERSION oppdateres av scripts/update-version.js
-const CACHE_VERSION = '2025-12-06+182614-53991ec';
+const CACHE_VERSION = '2025-12-06+183000-3188b59';
 
 const PRECACHE_PATHS = [
   './',
